27f3764e83a00b3c80fcc4c8224bb626d4652b76
smruti ranjan <smruti.barik@gmail.com>
2024-03-03 00:19:19 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDBContext dbContext;
+
+        public RegionsController(NZWalksDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions=dbContext.Regions.ToList(); 
+            return Ok(regions);
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] studentName = new string[] { "Smruti","mansi","saumya","abhisheka" };
+            return Ok(studentName);
+        }
+    }
+}
